00da8d3e09255811569da3869732ee9b1b264235
Josh Aldag <jraldag@gmail.com>
2024-06-11 11:47:42 -0500

    Update to question #3 to actually return the max number

--- prework_assignments_python_questions.py
+++ prework_assignments_python_questions.py
@@ -17,8 +17,8 @@ def first_odds():
 # Please write a Python function, max_num_in_list to return the max number of
     # a given list. The first line of the code has been defined below.
 def max_num_in_list(a_list):
-    max_num_in_list = (max(a_list))
-
+    max_num = max(a_list)
+    return(max_num)
 
 # Question 4
 # Write a function to return if the given year is a leap year. A leap year is
